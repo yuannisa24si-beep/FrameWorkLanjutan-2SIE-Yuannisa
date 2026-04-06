255eb55ce5ff1a9d648c02f24f2e61694843dd13
yuannisa <yuannisa24si@mahasiswa.pcr.ac.id>
2026-04-06 16:26:57 +0700

    Pertemuan4

--- src/Pertemuan4/main.jsx
+++ src/Pertemuan4/main.jsx
@@ -1,11 +1,14 @@
 import { createRoot } from "react-dom/client";
 import "./tailwind.css";
 import FrameworkList from "./FrameworkList";
+import FrameworkListSearchFilter from "./FrameworkListSearchFilter";
+import ResponsiveText from "./ResponsiveDesign";
 
 createRoot(document.getElementById("root"))
     .render(
         <div>
-        <FrameworkList />
+        {/* <FrameworkList /> */}
+        {/*FrameworkListSearchFilter/>*/}
+        <ResponsiveText />
         </div>
     );
-     
