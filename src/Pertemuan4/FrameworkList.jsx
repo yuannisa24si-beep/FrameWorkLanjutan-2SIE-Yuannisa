ed9937d66343f638e8133be164babd75767b691e
yuannisa <yuannisa24si@mahasiswa.pcr.ac.id>
2026-04-06 15:04:34 +0700

    updatePertemuan4

---
+++ src/Pertemuan4/FrameworkList.jsx
@@ -0,0 +1,35 @@
+import frameworkData from "./framework.json";
+
+export default function FrameworkList() {
+  return (
+    <div className="p-8">
+      {frameworkData.map((item) => (
+        <div
+          key={item.id}
+          className="border p-4 mb-4 rounded-lg shadow-md bg-white"
+        >
+          <h2 className="text-lg font-bold text-gray-800">{item.name}</h2>
+          <p className="text-gray-600">{item.description}</p>
+          <p className="text-red-600">{item.details.developer}</p>
+          <a
+            href={item.details.officialWebsite}
+            target="_blank"
+            className="text-blue-600 underline"
+          >
+            {item.details.officialWebsite}
+          </a>
+          <br></br>
+          {item.tags.map((tag, index) => (
+            <div key={index} className="bg-red-300 mb-1 text-center
+            hover:rotate-1  
+            text-gray-700 px-2 py-1 
+            text-xs rounded-full mr-2">
+              {tag}
+            </div>
+            
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+}
---
+++ src/Pertemuan4/main.jsx
@@ -0,0 +1,11 @@
+import { createRoot } from "react-dom/client";
+import "./tailwind.css";
+import FrameworkList from "./FrameworkList";
+
+createRoot(document.getElementById("root"))
+    .render(
+        <div>
+        <FrameworkList />
+        </div>
+    );
+     
