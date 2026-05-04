3f3da4a0beb7a5384e7bce804c1e3e19bf54bc57
yuannisa24si-beep <yuannisa24si@mahasiswa.pcr.ac.id>
2026-05-04 15:51:05 +0700

    Percobaan8final

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // appType: 'mpa',
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        pertemuan2: resolve(__dirname, 'Pertemuan-2.html'),
-        pertemuan3: resolve(__dirname, 'Pertemuan3.html'),
-        tugasp3: resolve(__dirname, 'Tugasp3.html'),
-      },
-    },
-  },
 })
